681d17a398ea87ce58d6a5dbd1bd94e1435afb6e
Anvash <syed.anwash@gmail.com>
2026-08-19 01:27:04 +0530

    fix: remove duplicate backend config tracking old bucket

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "udaan-batch11-bucket"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     use_lockfile = true
