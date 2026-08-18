2a5d7fce8c62b35e4952aaed27eafd870e77fed1
Anvash <syed.anwash@gmail.com>
2026-08-19 00:39:51 +0530

    Initial commit - Terraform Jenkins infrastructure

---
+++ Jenkins-Server-TF/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket       = "udaan-batch11-bucket"
+    region       = "us-west-2"
+    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+    encrypt      = true
+    use_lockfile = true
+  }
+  required_version = ">=1.13.3"
+  required_providers {
+    aws = {
+      version = ">= 6.23.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
---
+++ Tetris-V2/Process.md
@@ -0,0 +1,5 @@
+# Process.md
+
+1.Create Jenkins/EC2 machines via TF: Completed
+2. Configure Jenkins for AWS integrations : Completed
+3.Create the Job and deploy
